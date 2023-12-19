b00cadf9049268ed8a358e510fa611e6659faba4
vbroda17 <vbroda@vols.utk.edu>
2023-12-19 02:14:23 -0500

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let sum = 0;
+    if(!Number.isInteger(first) || !Number.isInteger(last)) return "ERROR"
+    if(first < 0 || last < 0) return "ERROR";
+    if(last < first) [first, last] = [last, first];
+    for(let i = first; i <= last; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
